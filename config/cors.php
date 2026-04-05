b314a8eadf67cf4eaec8e3b6e6981432c713e1f6
johnseyi <leojohnseyi@gmail.com>
2026-04-05 12:27:21 +0300

    Update CORS config for Vercel frontend
    - Allow http://localhost:3000 and https://okelcor-website.vercel.app
    - Set allowed_headers to ['*'] and supports_credentials to true
    - Remove env-var-based origin config

--- config/cors.php
+++ config/cors.php
@@ -12,24 +12,19 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => array_filter(array_map('trim', explode(',', env(
-        'CORS_ALLOWED_ORIGINS',
-        'http://localhost:3000,https://okelcor.de,https://www.okelcor.de'
-    )))),
+    'allowed_origins' => [
+        'http://localhost:3000',
+        'https://okelcor-website.vercel.app',
+    ],
 
     'allowed_origins_patterns' => [],
 
-    'allowed_headers' => [
-        'Content-Type',
-        'Authorization',
-        'Accept',
-        'X-Requested-With',
-    ],
+    'allowed_headers' => ['*'],
 
     'exposed_headers' => [],
 
-    'max_age' => 86400,
+    'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 
 ];
